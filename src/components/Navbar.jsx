88ad1d422c7774c3b6fee96c40e03b1e872e98d1
daniel-delrosario <delrosario.danielallen@gmail.com>
2022-09-26 22:18:14 +0800

    navbar resume new tab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -79,7 +79,7 @@ const Navbar = () => {
                     </p>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href="/shorturl.at/azARZ">
+                    <a className='flex justify-between items-center w-full text-gray-300' href="https://shorturl.at/azARZ" target="_blank">
                         Resume <BsFillPersonLinesFill size={30} />
                     </a>
                 </li>
